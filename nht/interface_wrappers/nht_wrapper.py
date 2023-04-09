6afb450022f1191bfe266dc773cca41baa8870d9
Kerrick Johnstonbaugh <kerrick@cedar1.cedar.computecanada.ca>
2023-04-08 18:44:36 -0700

    initial tensorflow wrapper for nht

---
+++ nht/interface_wrappers/nht_wrapper.py
@@ -0,0 +1,61 @@
+from gym import utils as gym_utils
+from gym import spaces
+import numpy as np
+from gym.envs.registration import register
+import gym
+from gym.envs.registration import spec, load
+from nht.utils import load_interface
+import tensorflow as tf
+from baselines.common.tf_util import get_session
+from nht.NHT import NHT
+
+def register_NHT_env(base_env, NHT_path):
+    
+    temp_env = gym.make(base_env)
+
+    register(
+        id=f'NHT_{base_env}',
+        entry_point='nht.mujoco_interface:NHTwrapper',
+        kwargs={'env': temp_env, 'NHT_path': NHT_path},
+    )
+
+
+class NHTwrapper(gym.Wrapper):
+    def __init__(self, env, NHT_path, action_dim):
+        super().__init__(env)
+        
+        self.action_dim = action_dim
+        cond_size = self.env.observation_space.shape
+        u_dim = self.env.action_space.shape
+
+        self.tfsess = get_session()
+        # placeholders to construct NHT computational graph
+        self.cond_inp = tf.placeholder(shape=[None, cond_size], dtype=tf.float32)
+        self.z = tf.placeholder(shape=[None, action_dim], dtype=tf.float32)
+
+        # NHT model (outputs basis for actuation subspace given current observation)
+        self.NHT_model = NHT(action_dim=action_dim, output_dim=u_dim, cond_dim=cond_size)
+
+        
+        self.NHT_model.h.net = tf.keras.models.load_model(NHT_path) # loads weights
+        self.Q_hat = tf.stop_gradient(self.NHT_model._get_map(self.cond_inp))
+        self.NHT_model.freeze_model()
+
+
+    def set_action_space(self):
+        n = self.action_space.shape[0]
+        self.action_space = spaces.Box(low=-np.sqrt(n/self.action_dim), high=np.sqrt(n/self.action_dim), shape=(self.action_dim,), dtype=np.float32)
+
+    def step(self, action):
+
+        k = self.action_dim
+        assert action.shape == (k,)
+
+        c = np.expand_dims(self._get_obs().copy(),0)
+        Q_hat = self.tfsess.run(self.Q_hat, feed_dict={self.cond_inp: c})
+        a = np.expand_dims(action.copy(),1) # turn action from agent to column vector tensor (with batch dimension)
+        u = np.matmul(Q_hat.squeeze(0), a).squeeze()
+
+        action = u.copy()
+
+        return self.env.step(action)
---
+++ test_scripts/test_wrapper.py
@@ -0,0 +1,3 @@
+from nht.interface_wrappers.nht_wrapper import register_NHT_env
+
+register_NHT_env('HalfCheetah-v4')
