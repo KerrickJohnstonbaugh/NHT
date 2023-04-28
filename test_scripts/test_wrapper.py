38ae2bf8517a20a54f9db9eff56fae8fe9ab95ab
Kerrick Johnstonbaugh <kerrick@cedar1.cedar.computecanada.ca>
2023-04-27 20:23:53 -0700

    commiting param changes for history

--- test_scripts/test_wrapper.py
+++ test_scripts/test_wrapper.py
@@ -1,6 +1,8 @@
 from nht.interface_wrappers.nht_wrapper import register_NHT_env
 import gym
 
-register_NHT_env('HalfCheetah-v4', 'NHT/test_scripts/map_model/NHT-L_10')
+register_NHT_env('Walker2d-v2', 'NHT/test_scripts/map_model/NHT-L_10', action_dim=2)
 
-my_NHT_env = gym.make('NHT_HalfCheetah-v4')
+my_NHT_env = gym.make('NHT_Walker2d-v2')
+print(my_NHT_env.reset())
+print(my_NHT_env.step([0, 0]))
